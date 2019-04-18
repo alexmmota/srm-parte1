3aa33dd65e18190eb9f32e55d89141e20f848238
Alex Mota <alexmota5@hotmail.com>
2019-04-17 22:41:23 -0300

    fix bug

--- estoque/src/main/java/br/com/srm/model/Product.java
+++ estoque/src/main/java/br/com/srm/model/Product.java
@@ -1,5 +1,6 @@
 package br.com.srm.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
 import javax.persistence.*;
@@ -12,14 +13,17 @@ import java.io.Serializable;
 public class Product implements Serializable {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
+
+    @Column(name = "bar_code")
     private String barCode;
     private String department;
     private Integer amount;
 
+    @JsonIgnore
     @Transient
     public boolean isNew() {
         return id == null;
--- estoque/src/main/resources/db/migration/V1.0__CREATE_TABLES.sql
+++ estoque/src/main/resources/db/migration/V1.0__CREATE_TABLES.sql
@@ -4,7 +4,7 @@
 CREATE TABLE IF NOT EXISTS `db_srm`.`srm_product` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(400) NULL,
-  `barcode` VARCHAR(200) NULL,
+  `bar_code` VARCHAR(200) NULL,
   `department` VARCHAR(100) NULL,
   `amount` INT NULL,
   PRIMARY KEY (`id`))
